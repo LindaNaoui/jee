6bc236aab16c6448ce944b211ce7359905a5c689
LindaNaoui <linda.naoui@esprit.tn>
2019-05-08 11:41:45 +0100

    client

--- projetpaas/projetpaas-ejb/src/main/java/entities/Clients.java
+++ projetpaas/projetpaas-ejb/src/main/java/entities/Clients.java
@@ -14,7 +14,7 @@ public class Clients  {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	 private int Clientid;
-//h
+//hj
     private String Nom ;
     private String Prenom ;
     private String Email ;
